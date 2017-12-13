d0ad0ac2ea3c20178138cf30aa1169d750d0cd56
Samuel LYNN-EVANS <slynn-ev@e3r6p16.42.fr>
2017-12-13 22:14:10 +0100

    working, but slowly

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "fillit.h"
+#include <stdio.h>
 
 void build_array(int *tlib, char *s)
 {
@@ -51,7 +52,7 @@ int check_format(char *buff)
 			return (-1);
 		else if ((i - offset) % 5 == 0 && buff[i] != '\n')
 			return (-1);
-		else if (buff[i] != '#' && buff[i] != '.' && buff[i] != '\n')
+		else if (buff[i] != '#' && buff[i] != '.' && (i + 1) % 21 != 0 && (i - offset) % 5 != 0)
 			return (-1);
 		if ((i + 1) % 21 == 0)
 			offset++;
@@ -88,15 +89,6 @@ int	get_coordinates(char *buff, int map_count, int coordinates[map_count][8])
 			i = -1;
 		}
 	}
-	j = 0;
-	while (j < map_count)
-	{
-		i = 0;
-		while (i < 8)
-			ft_putnbr(coordinates[j][i++]);
-		ft_putchar('\n');
-		j++;
-	}
 	return ((c == 8) ? 1 : 0);	
 }
 
@@ -111,8 +103,8 @@ int	check_for_match(int *input_coordinates, int *tlib_coordinates)
 	i = 0;
 	while (i < 8)
 	{
-		input_coordinates[i] = input_coordinates[i] - x;
-		input_coordinates[i + 1] = input_coordinates[i + 1] - y;
+		input_coordinates[i] -= x;
+		input_coordinates[i + 1] -= y;
 		i += 2;
 	}
 	i = 0;
@@ -154,35 +146,108 @@ int	**assign_tetriminos(int map_count, int coordinates[map_count][8], int tlib[1
 	return (tetriminos);
 }
 
-/*int	backtrack_filler(int t, int tetriminos, int tlib[19][8], int **square)
+int	put_tetrimino(int *tetrimino, char **square, int row, int col, int alpha, int size)
 {
-			
-}*/
-/*
-void	solver(int map_count, int *tetriminos, int size)
+	int	i;
+	int x;
+	int	y;
+
+	i = 0;
+	while (i < 8)
+	{
+		x = col + tetrimino[i];
+		y = row + tetrimino[i + 1];
+		if (y >= size || x >= size || square[y][x] != '.')
+			return (0);
+		i += 2;
+	}
+	i = 0;
+	while (i < 8)
+	{
+		x = col + tetrimino[i];
+		y = row + tetrimino[i+1];
+		square[y][x] = alpha + 'A';
+		i += 2;
+	}
+	return (1);
+}
+
+void	remove_tetrimino(int *tetrimino, char **square, int row, int col)
 {
-	int **square;
-	int	row;
-	int col;
+	int	i;
+	int x;
+	int	y;
+	
+	i = 0;
+	while (i < 8)
+	{
+		x = col + tetrimino[i];
+		y = row + tetrimino[i+1];
+		square[y][x] = '.';
+		i += 2;
+	}
+}
+
+int	backtrack_filler(int i, int **tetriminos, char **square, int row, int col, int size)
+{
+	if (col >= size)
+	{
+		row++;
+		col = 0;
+	}	
+	while (row < size)
+	{
+		if (put_tetrimino(tetriminos[i], square, row, col, i, size))
+		{
+			if (tetriminos[i + 1] == 0)
+				return (1);
+			else if (backtrack_filler(i + 1, tetriminos, square, row, col + 1, size))
+				return (1);
+			else
+				remove_tetrimino(tetriminos[i], square, row, col);
+		}
+		col++;
+		if (col >= size)
+		{
+			row++;
+			col = 0;
+		}	
+	}
+	return (0);
+}
+
+int	solver(int **tetriminos, int size)
+{
+	char	**square;
+	int		row;
+	int		col;
 
 	row = 0;
-	square = malloc(sizeof(int *) * size);
+	square = malloc(sizeof(char *) * (size + 1));
 	while (row < size)
 	{
 		col = 0;
-		square[row] = malloc(sizeof(int) * size);
+		square[row] = malloc(sizeof(char) * size);
 		while (col < size)
 			square[row][col++] = '.';
+		square[row][col] = '\0';
 		row++;
 	}
-//	if (!backtrack_filler(0, tetriminos, tlib, square))
-//	{
-//		free(square);
-//		return (0);
-//	}
+	square[row] = 0;
+	if (!backtrack_filler(0, tetriminos, square, 0, 0, size))
+	{
+		free(square);
+		return (0);
+	}
+	row = 0;
+	while (row < size)
+	{
+		ft_putstr(square[row++]);
+		ft_putchar('\n');
+	}
 	return (1);
 }
-*/
+
 int		setup_solver(char *buff, int map_count)
 {
 	int			tlib[19][8];
@@ -196,10 +261,10 @@ int		setup_solver(char *buff, int map_count)
 	build_tetrimino_library(tlib);	
 	if (!(tetriminos = assign_tetriminos(map_count, coordinates, tlib)))
 		return (-1);
-//	while (!solver(tetriminos, map_count, size))
-//		size++;
+	while (!solver(tetriminos, size))
+		size++;
 		
-//	return (1);
+	return (1);
 }
 
 int	main(int ac, char **argv)
@@ -214,7 +279,7 @@ int	main(int ac, char **argv)
 		fd = open(argv[1], O_RDONLY);
 		ret = read(fd, buff, BUFF_SIZE);
 		buff[ret] = '\0';	
-		if ((map_count = check_format(buff)) == -1 || map_count > 25)
+		if ((map_count = check_format(buff)) == -1 || map_count > 26)
 		{	
 			ft_putstr("error");
 			return (1);
